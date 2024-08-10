cd29a3886f16d7b3c39ffa452c9d5c2433592797
Juanignacioz <jzotelo@estudiantes.unsam.edu.ar>
2024-08-10 12:57:48 -0400

    se agrega estructura del proyecto

---
+++ src/main/kotlin/com/example/clubapp/ClubappApplication.kt
@@ -0,0 +1,11 @@
+package com.example.clubapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClubappApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClubappApplication>(*args)
+}
